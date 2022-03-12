3ae3628de3427891662ff6b5df2f5cfe55e7aeca
Joe Curtis <joecurtis9999@gmail.com>
2022-03-12 18:29:32 +0000

    added routes to the html notes file and a route to read the db.json file

--- server.js
+++ server.js
@@ -4,3 +4,15 @@ const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 8000
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+});
